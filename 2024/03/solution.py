c0c4349e3f0fb47b218e6c5a38248250f118ed9a
laven-dev <77447729+laven-dev@users.noreply.github.com>
2025-05-24 16:19:25 +0930

    day 3

---
+++ 2024/03/solution.py
@@ -0,0 +1,31 @@
+import sys
+import re
+with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt") as f:
+    data = "".join(f.read().splitlines())
+
+mulList = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", data)
+# print(mulList)
+def mult(data: str) -> int:
+    data = data.removeprefix("mul(")
+    data = data[:-1]
+    left, right = [int(x) for x in data.split(",")]
+    result = left * right
+    return result
+
+silver = 0
+gold = 0
+yeppers = True
+for item in mulList:
+    if item == "do()":
+        yeppers = True
+        continue
+    if item == "don't()":
+        yeppers = False
+        continue
+
+    if yeppers:
+        gold += mult(item)
+    
+    silver += mult(item)
+
+print(f"Silver: {silver}, Gold: {gold}")
